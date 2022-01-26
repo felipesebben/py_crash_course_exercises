502939552aef3b638475388eee60db3c86afc431
Felipe Sebben <felipesebben@yahoo.com.br>
2022-01-26 19:26:27 -0300

    Updated exercise 10-11.

--- 10_files_and_exceptions/10-11. Favorite Number.py
+++ 10_files_and_exceptions/10-11. Favorite Number.py
@@ -2,3 +2,11 @@
 # Use json.dump() to store this number in a file. Write a separate program
 # that reads in this value and prints the message,
 # “I know your favorite number! It’s _____.”
+import json
+
+favorite_number = input("Tell me your favorite number: ")
+
+filename = 'favorite_number.json'
+with open(filename, 'w') as f:
+    json.dump(favorite_number, f)
+    print(f"I'll remember that your favorite number is {favorite_number}!")
