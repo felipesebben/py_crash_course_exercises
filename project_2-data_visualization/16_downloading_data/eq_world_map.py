b18f12ef0c253e0937c4d9a67722db68417c8358
Felipe Sebben <felipesebben@yahoo.com.br>
2022-03-02 16:33:31 -0300

    Update app with earthquake mags marked in colormap

--- project_2-data_visualization/16_downloading_data/eq_world_map.py
+++ project_2-data_visualization/16_downloading_data/eq_world_map.py
@@ -6,10 +6,11 @@ from plotly.graph_objs import Scattergeo, Layout
 from plotly import offline
 import os
 
-os.chdir('C:/Users/Felipe/python_work/notebooks/bk_Python_crash_course/project_2-data_visualization/16_downloading_data')
+os.chdir('C:/Users/Felipe/python_work/notebooks/bk_Python_crash_course/' +
+         'project_2-data_visualization/16_downloading_data')
 
 
-filename = 'data/eq_data_1_day_m1.json'
+filename = 'data/eq_data_30_day_m1.json'
 with open(filename) as f:
     all_eq_data = json.load(f)
 
@@ -24,8 +25,21 @@ for eq_dict in all_eq_dicts:
     lons.append(lon)
     lats.append(lat)
 
-# Map the earthquakes.
-data = [Scattergeo(lon=lons, lat=lats)]  # Create a list with Scattergo object.
+# Map the earthquakes cursomizing marker size according to mag sizes.
+# Create a dict with Scattergo object.
+data = [{
+    'type': 'scattergeo',
+    'lon': lons,
+    'lat': lats,
+    'marker': {
+        'size': [4*mag for mag in mags],
+        'color': mags,
+        'colorscale': 'Viridis',
+        'reversescale': True,
+        'colorbar': {'title': 'Magnitude'}
+    },
+}]
+
 my_layout = Layout(title='Global Earthquakes')  # Title
 
 fig = {'data': data, 'layout': my_layout}  # Dictionary with data and layout.
