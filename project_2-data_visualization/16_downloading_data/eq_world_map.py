1f2a0bacdae8237417febaeb52ada248a2d66ef0
Felipe Sebben <felipesebben@yahoo.com.br>
2022-03-03 07:41:39 -0300

    Add hover features to app

--- project_2-data_visualization/16_downloading_data/eq_world_map.py
+++ project_2-data_visualization/16_downloading_data/eq_world_map.py
@@ -16,14 +16,17 @@ with open(filename) as f:
 
 all_eq_dicts = all_eq_data['features']
 
-mags, lons, lats = [], [], []
+mags, lons, lats, hover_texts = [], [], [], []
 for eq_dict in all_eq_dicts:
     mag = eq_dict['properties']['mag']
     lon = eq_dict['geometry']['coordinates'][0]
     lat = eq_dict['geometry']['coordinates'][1]
+    title = eq_dict['properties']['title'] # Add hover text.
     mags.append(mag)
     lons.append(lon)
     lats.append(lat)
+    hover_texts.append(title)
+
 
 # Map the earthquakes cursomizing marker size according to mag sizes.
 # Create a dict with Scattergo object.
@@ -31,10 +34,11 @@ data = [{
     'type': 'scattergeo',
     'lon': lons,
     'lat': lats,
+    'text': hover_texts,
     'marker': {
-        'size': [4*mag for mag in mags],
+        'size': [3*mag for mag in mags],
         'color': mags,
-        'colorscale': 'Viridis',
+        'colorscale': 'Electric',
         'reversescale': True,
         'colorbar': {'title': 'Magnitude'}
     },
